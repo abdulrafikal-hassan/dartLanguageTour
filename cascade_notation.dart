c0c02cfa4fb5bacd09c24c07a6d892bea9cdf2a8
babyabdul <aral-hassan001@st.ug.edu.gh>
2021-11-30 10:24:27 +0000

    Dart Cascade Notation following the Dart Laguage Tour Docs

---
+++ cascade_notation.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("This file is about Dart Cascade notation");
+  print(
+      "A cascade is created by using the '..' and '?..' for null-shorting cascade");
+
+  // var sb = StringBuffer();
+  // sb.write('foo')
+  // ..write('bar');
+}
